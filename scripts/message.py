035e31fc2922d4fbf1875ba79914c015bb16b52d
kalaiprakashgit <kalaiprakash@gmail.com>
2018-10-07 09:02:19 -0400

    script to replace file content

---
+++ scripts/message.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+message = raw_input("Enter the message : ")
+count = raw_input("Number of repeats: ").strip()
+
+if count:
+    count = int(count)
+else:
+    count = 1
+
+
+def multi_echo(message, count):
+    while count > 0:
+        print (message)
+        count -= 1
+
+multi_echo(message, count)
